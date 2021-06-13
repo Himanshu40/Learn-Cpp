de5701da1002fbf675a9414da4d9d0484825a6e7
Himanshu40 <himanshuwindows8.1@gmail.com>
2021-06-13 09:29:52 +0530

    added inital functions

--- Section13/Challenge/Movies.h
+++ Section13/Challenge/Movies.h
@@ -1,10 +1,25 @@
+/******************************************************************
+ * Section 13 Challenge
+ * Movies.h
+ * 
+ * Models a collection of Movie as a std::vector
+ *  implement these methods in Movies.cpp
+ *
+ * ***************************************************************/
+
 #ifndef _MOVIES_H_
 #define _MOVIES_H_
 
+#include <vector>
+#include <string>
+#include "Movie.h"
+
 class Movies {
-        friend class Movie;
+        std::vector<Movie> movies;
     public:
-        Movies();
+        bool incrementWatched(std::string name);
+        bool addMovie(std::string name, std::string rating, int watched);
+        void display() const;
 };
 
 #endif
--- Section13/Challenge/main.cpp
+++ Section13/Challenge/main.cpp
@@ -1,9 +1,59 @@
+/******************************************************************
+ * Section 13 Challenge
+ * main.cpp
+ * 
+ * Test the Movies project
+ * 
+ * ***************************************************************/
+
 #include <iostream>
 #include "Movies.h"
 
-using namespace std;
+
+void incrementWatched(Movies &movies, std::string name);
+void addMovie(Movies &movies, std::string name, std::string rating, int watched);
+
 
 int main() {
+    Movies myMovies;
+    
+    myMovies.display();
+    
+    addMovie(myMovies, "Big", "PG-13",2);     // OK
+    addMovie(myMovies,"Star Wars", "PG",5);   // OK
+    addMovie(myMovies,"Cinderella", "PG",7);  // OK
+     
+    myMovies.display();                       // Big, Star Wars, Cinderella
+    
+    addMovie(myMovies,"Cinderella", "PG",7);  // Already exists
+    addMovie(myMovies,"Ice Age", "PG",12);    // OK
+ 
+    myMovies.display();                       // Big, Star Wars, Cinderella, Ice Age
+    
+    incrementWatched(myMovies,"Big");         // OK
+    incrementWatched(myMovies,"Ice Age");     // OK
+    
+    myMovies.display();                       // Big and Ice Age watched count incremented by 1
+    
+    incrementWatched(myMovies,"XXX");         // XXX not found
 
     return 0;
+}
+
+void incrementWatched(Movies &movies, std::string name) {
+    if (movies.incrementWatched(name)) {
+        std::cout << name << " watch incremented" <<  std::endl;
+    }
+    else {
+        std::cout << name << " not found" <<  std::endl;
+    }
+}
+
+void addMovie(Movies &movies, std::string name, std::string rating, int watched) {
+    if (movies.addMovie(name, rating, watched)) {
+        std::cout << name << " added" << std::endl;
+    }
+    else {
+        std::cout << name << " already exists" <<  std::endl;
+    }
 }
