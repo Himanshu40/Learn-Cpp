75f20650cea3e361141770159889b2e117793c04
Himanshu40 <himanshuwindows8.1@gmail.com>
2021-06-14 07:16:17 +0530

    Added overloading operators of binary and unary

---
+++ Section14/OverloadingOperators/MyString.h
@@ -0,0 +1,33 @@
+#ifndef _MY_STRING_H_
+#define _MY_STRING_H_
+
+
+class MyString {
+        // Pointer to char[] that hold a C-Style String
+        char *str;
+    public:
+        MyString();
+        MyString(const char *s);
+        MyString(const MyString &src);
+        MyString(MyString &&src);
+        ~MyString();
+
+        // Copy assignment
+        MyString &operator=(const MyString &rhs);
+        // Move assignment
+        MyString &operator=(MyString &&rhs);
+
+        // Make lowercase string
+        MyString operator-() const;
+        // Concatenate string
+        MyString operator+(const MyString &rhs) const;
+        // Check if two strings are equal
+        MyString operator==(const MyString &rhs) const;
+
+        void display() const;
+        int getLength() const;
+        const char *getStr() const;
+};
+
+
+#endif
