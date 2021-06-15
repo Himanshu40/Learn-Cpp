3283df42b731149f84f7db704aedb67e88e31d03
Himanshu40 <himanshuwindows8.1@gmail.com>
2021-06-15 10:03:07 +0530

    Added some space between initialization

--- Section13/delegateConstructors.cpp
+++ Section13/delegateConstructors.cpp
@@ -21,7 +21,7 @@ Player::Player(string nameVal) : Player {nameVal, 0, 0} {
     cout << "One-args constructor" << endl;
 }
 
-Player::Player(string nameVal, int healthVal, int xpVal) : name{nameVal}, health{healthVal}, xp{xpVal} {
+Player::Player(string nameVal, int healthVal, int xpVal) : name {nameVal}, health {healthVal}, xp {xpVal} {
     cout << "Three-args constructor" << endl;
 }
 
