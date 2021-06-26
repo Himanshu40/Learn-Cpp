4d487e316af0c8ddc898c0991f1889d0b841bf71
Himanshu40 <himanshuwindows8.1@gmail.com>
2021-06-26 11:52:13 +0530

    Added class Account prototype

---
+++ Section15/Challenge/Account.h
@@ -0,0 +1,24 @@
+#ifndef _ACCOUNT_H_
+#define _ACCOUNT_H_
+
+#include <iostream>
+#include <string>
+
+
+class Account {
+    private:
+        static constexpr const char *defName = "Unnamed Account";
+        static constexpr double defBalance = 0.0;
+        friend std::ostream &operator<<(std::ostream &os, const Account &acc);
+    protected:
+        std::string name;
+        double balance;
+    public:
+        Account(std::string name = defName, double balance = defBalance);
+        bool deposit(double amount);
+        bool withdraw(double amount);
+        double getBalance() const;
+};
+
+
+#endif
