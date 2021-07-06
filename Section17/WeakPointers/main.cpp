a527af0aa7ce5d51ff54505e34625f99b62b1d88
Himanshu40 <himanshuwindows8.1@gmail.com>
2021-07-07 00:51:15 +0530

    Added Weak pointers

---
+++ Section17/WeakPointers/main.cpp
@@ -0,0 +1,41 @@
+// Problem with shared ownership in Circular or Cyclic relationship
+#include <iostream>
+#include <memory>
+
+class B;            // forward declaration
+
+class A {
+        std::shared_ptr<B> bPtr;
+    public:
+        void setB(std::shared_ptr<B> &b) {
+            bPtr = b;
+        }
+
+        A() {std::cout << "A Constructor" << std::endl;}
+
+        ~A() {std::cout << "A Destructor" << std::endl;}
+};
+
+class B {
+        std::shared_ptr<A> aPtr;
+    public:
+        void setA(std::shared_ptr<A> &a) {
+            aPtr = a;
+        }
+
+        B() {std::cout << "B constructor" << std::endl;}
+
+        ~B() {std::cout << "B Destructor" << std::endl;}
+};
+
+int main() {
+    std::shared_ptr<A> a = std::make_shared<A>();
+    std::shared_ptr<B> b = std::make_shared<B>();
+
+    a->setB(b);
+    b->setA(a);
+
+    // memory leaked as destructor never being called
+
+    return 0;
+}
